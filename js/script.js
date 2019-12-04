99de608ba714d0f9ab673f418e593954262d9670
Radoslaw Zaborski <zaborski.radoslaw@gmail.com>
2019-12-04 15:50:22 +0100

    add result game system in script.js

--- js/script.js
+++ js/script.js
@@ -32,14 +32,47 @@ if(randomNumber == 1) {
 
  let playerMove = 'nieznany ruch';
 
- if (playerInput == '1') {
+ 	if (playerInput == '1') {
 		 playerMove = 'kamień';
- }
-	 else if (playerInput == '2'){
+ 	}
+	else if (playerInput == '2'){
 		 playerMove = 'papier';
-	 }
-	 else if (playerInput == '3'){
+	}
+	else if (playerInput == '3'){
 		 playerMove = 'nożyce';
 	 
-	 }
-	 printMessage ('twój ruch to:' + playerMove);
+	}
+	
+ printMessage ('twój ruch to:' + playerMove);
+
+ if ( computerMove == 'kamień' && playerMove == 'papier'){
+ printMessage ('Wygrałeś');
+ }
+else if ( computerMove == 'kamień' && playerMove == 'nożyce'){
+printMessage('przegrałeś');
+}
+else if ( computerMove == 'kamień' && playerMove == 'kamień'){
+printMessage('remis');
+}
+else if ( computerMove == 'papier' && playerMove == 'kamień'){
+printMessage('przegrałeś');
+}
+else if ( computerMove == 'papier' && playerMove == 'papier'){
+printMessage('remis');
+}
+else if ( computerMove == 'papier' && playerMove == 'nożyce'){
+printMessage('Wygrałeś');
+}
+else if ( computerMove == 'nożyce' && playerMove == 'kamień'){
+printMessage('wygrałeś');
+}
+else if ( computerMove == 'nożyce' && playerMove == 'papier'){
+printMessage('przegrałeś');
+}
+else if ( computerMove == 'nożyce' && playerMove == 'nożyce'){
+printMessage('remis');
+}
+else {
+	printMessage('coś poszło nie tak, spróbuj jeszcze raz wpisując poprawną wartość');
+}
+	
